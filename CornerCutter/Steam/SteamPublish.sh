5f8d8006585eef5e6c84582e575b2a4d7a91886f
FatcatX <garretsimpson@yahoo.com>
2026-04-11 14:24:54 -0700

    Modified publish script

--- CornerCutter/Steam/SteamPublish.sh
+++ CornerCutter/Steam/SteamPublish.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-CONTENT_PATH=$1
-
-# Prints the current working directory into the variable while converting the POSIX-style path to windows-style path
-# Converts /c/ to C:/
-CURRENT_DIR=$(cygpath -w "$PWD")
+#CONTENT_PATH=$1
+CONTENT_PATH="/mnt/c/Users/garre/AppData/LocalLow/tobspr Games/shapez 2/mods/CornerCutter"
 
 # Composes the location of the preview image
-PREVIEW_IMG=$CURRENT_DIR\\Steam\\preview.png
-
-# Adjust paths to use double backlashes
-CONTENT_PATH="${CONTENT_PATH//\\/\\\\}"
-PREVIEW_IMG="${PREVIEW_IMG//\//\\}"
-PREVIEW_IMG="${PREVIEW_IMG//\\/\\\\}"
+PREVIEW_IMG="${PWD}/Steam/preview.png"
 
 echo "CONTENT_PATH: $CONTENT_PATH"
 echo "PREVIEW_IMG: $PREVIEW_IMG"
@@ -21,25 +13,24 @@ export CONTENT_PATH
 export PREVIEW_IMG
 
 # Adjust temporary .vdf with absolute paths for the content and the preview image
-envsubst < Steam\\base.vdf > Steam\\base.tmp.vdf
+TMP_VDF=Steam/mod.vdf
+envsubst < Steam/temp.vdf > $TMP_VDF
 
 # Log the final version
-cat Steam\\base.tmp.vdf
-
-TMP_VDF=$CURRENT_DIR\\Steam\\base.tmp.vdf
+cat $TMP_VDF
 
 # Execute
-steamcmd +login lorenzo_tobspr +workshop_build_item "$TMP_VDF" +quit;
+steamcmd +login fatcatx +workshop_build_item "$TMP_VDF" +quit;
 
 # Copy published file id back
-cat Steam\\base.tmp.vdf
+cat $TMP_VDF
 
 # Grab published file id 
-FILE_ID=$(grep '"publishedfileid"' Steam\\base.tmp.vdf | sed 's/.*"publishedfileid"[ \t]*"\([0-9]\+\)".*/\1/')
+FILE_ID=$(grep '"publishedfileid"' Steam/base.tmp.vdf | sed 's/.*"publishedfileid"[ \t]*"\([0-9]\+\)".*/\1/')
 
 # Updating original file with new published file ID
 echo "New published file ID: $FILE_ID"
-sed -i 's/\("publishedfileid"[ \t]*"\)[0-9]\+"/\1'"$FILE_ID"'"/'  Steam\\base.vdf
+sed -i 's/\("publishedfileid"[ \t]*"\)[0-9]\+"/\1'"$FILE_ID"'"/' Steam/base.vdf
 
 # Clean temporary files
-rm Steam\\base.tmp.vdf
+rm $TMP_VDF
