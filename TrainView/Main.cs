7b06f554ed7f4ae7df88e373a27807b97d68baeb
FatcatX <garretsimpson@yahoo.com>
2026-06-06 18:14:18 -0700

    Modify TrainView
    - Refactor Camera update to be in Main instead of the GameObject.

--- TrainView/Main.cs
+++ TrainView/Main.cs
@@ -1,15 +1,16 @@
 using Core.Dependency;
 using Cysharp.Threading.Tasks;
+using Game.Core.Trains;
 using Game.Orchestration;
 using MonoMod.RuntimeDetour;
-using Game.Core.Trains;
 using ShapezShifter.Flow;
 using ShapezShifter.Kit;
 using ShapezShifter.SharpDetour;
+using System.Reflection;
+using Unity.Collections;
 using UnityEngine;
 using static ShapezShifter.Flow.ModConsoleCommandsCreator;
 using ILogger = Core.Logging.ILogger;
-using Unity.Collections;
 
 namespace TrainView
 {
@@ -22,7 +23,10 @@ namespace TrainView
         private Hook GameInitHook;
         private Hook SessionInitHook;
         private Hook CameraHook;
+        private Hook TrainHook;
         private GameObject myGameObject;
+
+        private delegate bool DrawTrainWagonDelegate(TrainsDrawer self, FrameDrawOptionsNoLOD draw, TrainId trainId, TrainData train, int wagonIdx, WagonNavigationData wagonData, out Matrix4x4 wagonTrs);
 
         public static GlobalsData Globals;
         public static DependencyContainer GameDependencyContainer;
@@ -49,6 +53,11 @@ namespace TrainView
         {
             consoleRewirer.AddCommand(console =>
             {
+                console.Register("debug", context =>
+                {
+                    var debugmode = SessionDependencyContainer.Resolve<DebugModeManager>();
+                    debugmode.ShowView("trains");
+                });
                 console.Register("getview", context =>
                 {
                     var camera = SessionDependencyContainer.Resolve<CameraController>();
@@ -78,6 +87,8 @@ namespace TrainView
 
         public void CreateHooks()
         {
+            BindingFlags FLAGS = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
+
             GameInitHook = DetourHelper.CreatePostfixHook<GameOrchestrator, UniTask>(
                 original: orchestrator => orchestrator.InitializeMainMenu(),
                 postfix: GetGameData);
@@ -87,6 +98,10 @@ namespace TrainView
             CameraHook = DetourHelper.CreatePostfixHook<GameSessionOrchestrator, IGameData, CameraGameSettings, Keybindings>(
                 original: (orchestrator, gameData, cameraSettings, keybindings) => orchestrator.Init_6_PlayerInteraction(gameData, cameraSettings, keybindings),
                 postfix: BindCamera);
+            // Use low level hook call because ShapezShifter's detour doesn't support out parameters.
+            TrainHook = new Hook(
+                typeof(TrainsDrawer).GetMethod("DrawTrainWagon", FLAGS),
+                typeof(Main).GetMethod("UpdateTrainCamera", FLAGS), this);
         }
 
         private UniTask GetGameData(GameOrchestrator self, UniTask result)
@@ -104,11 +119,40 @@ namespace TrainView
             self.DependencyContainer.Bind<CameraController>().To(self.PlayerInteractionOrchestrator.CameraController);
         }
 
+        private bool UpdateTrainCamera(DrawTrainWagonDelegate orig, TrainsDrawer self, FrameDrawOptionsNoLOD draw, TrainId trainId, TrainData train, int wagonIdx, WagonNavigationData wagonData, out Matrix4x4 wagonTrs)
+        {
+            wagonTrs = default;
+            bool result = orig(self, draw, trainId, train, wagonIdx, wagonData, out wagonTrs);
+            if (result == false || wagonIdx != 0 || trainId != MyGameObject.CurrentTrainId)
+            {
+                return result;
+            }
+
+            var sim = GameHelper.Core.LocalPlayer.CurrentMap.Simulator;
+            var TrainSim = sim.GetSystem<TrainSystem>().TrainsSimulation;
+            var trainData = TrainSim.GetTrainData(MyGameObject.CurrentTrainId);
+            var camera = Main.SessionDependencyContainer.Resolve<CameraController>();
+            var (pos, rot) = TrainSimulationHelper.CalculateTrainPosition(trainData);
+            // convert to camera coordinates
+            pos.y = -pos.y;
+            camera.CurrentPosition = pos;
+
+            // Rotate the camera only if the camera is zoomed in.
+            var zoom = GameHelper.Core.Viewport.Zoom;
+            if (zoom < 75f)
+            {
+                camera.TargetRotationDegrees = rot;
+            }
+
+            return true;
+        }
+
         public void Dispose()
         {
             GameInitHook?.Dispose();
             SessionInitHook?.Dispose();
             CameraHook?.Dispose();
+            TrainHook?.Dispose();
             consoleRewirer?.Dispose();
 
             if (myGameObject != null)
--- TrainView/MyGameObject.cs
+++ TrainView/MyGameObject.cs
@@ -13,9 +13,7 @@ namespace TrainView
     {
         private static ILogger _logger;
 
-        private TrainId CurrentTrainId = TrainId.Invalid;
-        private NativeArray<TrainId> TrainIds;
-        private TrainsSimulation TrainSim;
+        public static TrainId CurrentTrainId = TrainId.Invalid;
 
         public static void SetLogger(ILogger logger)
         {
@@ -27,6 +25,8 @@ namespace TrainView
             // Check if train was destroyed and unbind if so.
             try
             {
+                var sim = GameHelper.Core.LocalPlayer.CurrentMap.Simulator;
+                var TrainSim = sim.GetSystem<TrainSystem>().TrainsSimulation;
                 TrainSim.GetTrainData(CurrentTrainId);
             }
             catch (Exception ex)
@@ -39,58 +39,30 @@ namespace TrainView
             if (UnityEngine.InputSystem.Keyboard.current != null &&
                 UnityEngine.InputSystem.Keyboard.current.commaKey.wasPressedThisFrame)
             {
-                PickTrain(-1);
+                CurrentTrainId = PickTrain(-1);
             }
             if (UnityEngine.InputSystem.Keyboard.current != null &&
                 UnityEngine.InputSystem.Keyboard.current.periodKey.wasPressedThisFrame)
             {
-                PickTrain(+1);
+                CurrentTrainId = PickTrain(+1);
             }
             if (UnityEngine.InputSystem.Keyboard.current != null &&
                 UnityEngine.InputSystem.Keyboard.current.slashKey.wasPressedThisFrame)
             {
                 CurrentTrainId = TrainId.Invalid;
-                return;
             }
-            if (CurrentTrainId == TrainId.Invalid)
-            {
-                return;
-            }
-
-            var trainData = TrainSim.GetTrainData(CurrentTrainId);
-            var camera = Main.SessionDependencyContainer.Resolve<CameraController>();
-            var (pos, rot) = TrainSimulationHelper.CalculateTrainPosition(trainData);
-            ConvertToCamera(ref pos, ref rot);
-            camera.CurrentPosition = pos;
-
-            // Rotate the camera only if the camera is zoomed in.
-            var zoom = GameHelper.Core.Viewport.Zoom;
-            if (zoom < 75f)
-            {
-                camera.TargetRotationDegrees = rot;
-            }
+            //_logger.Info.Log($"CurrentTrainId: {CurrentTrainId}");
         }
 
-        public static void ConvertToCamera(ref double2 pos, ref float rot)
+        private TrainId PickTrain(int dir)
         {
-            pos.y = -pos.y;
-        }
-
-        private void PickTrain(int dir)
-        {
-            if (TrainIds.IsCreated)
-            {
-                TrainIds.Dispose();
-            }
-
             var sim = GameHelper.Core.LocalPlayer.CurrentMap.Simulator;
-            TrainSim = sim.GetSystem<TrainSystem>().TrainsSimulation;
-            TrainIds = TrainSim.GetAllTrains(Allocator.Persistent);
+            var TrainSim = sim.GetSystem<TrainSystem>().TrainsSimulation;
+            var TrainIds = TrainSim.GetAllTrains(Allocator.Temp);
             //_logger.Info.Log($"TrainIds length {TrainIds.Length}");
             if (TrainIds.Length == 0)
             {
-                CurrentTrainId = TrainId.Invalid;
-                return;
+                return TrainId.Invalid;
             }
 
             int index;
@@ -103,16 +75,11 @@ namespace TrainView
                 index = TrainIds.IndexOf(CurrentTrainId);
                 index = (index + TrainIds.Length + dir) % TrainIds.Length;
             }
-            CurrentTrainId = TrainIds[index];
-            //Console.WriteLine($"CurrentTrainId: {CurrentTrainId}");
+            return TrainIds[index];
         }
 
         void OnDestroy()
         {
-            if (TrainIds.IsCreated)
-            {
-                TrainIds.Dispose();
-            }
         }
     }
 }
